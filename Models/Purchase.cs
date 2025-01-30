10612e96e0f9afdef473d7c2edcf3a98ace47608
Bentia <1548884@cegepmontpetit.ca>
2025-01-30 14:37:56 -0500

    relation

---
+++ Models/Purchase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace RelationsNaN.Models
+{
+    public class Purchase
+    {
+
+        public int Id { get; set; }
+        [ValidateNever]
+        public List<GamePurchase> GamePurchases { get; set; }
+
+    }
+}
